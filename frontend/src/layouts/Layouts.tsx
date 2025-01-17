1b7256caf2b0f73618ea50e01eef3bbbd373ee2f
Happy <happy@Happys-MacBook-Air.local>
2025-01-17 18:12:35 +0530

    Add frontend folder

---
+++ frontend/src/layouts/Layouts.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+
+
+const Layout = () => {
+    return  <div className="flex flex-col min-h-screen ">
+    <Header/>
+
+
+
+           
+           
+             
+            </div>
+};
+    
