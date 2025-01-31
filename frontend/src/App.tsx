9bf25454c8c00d3db7266a8d82941affff33d826
Happy <happy@Happys-MacBook-Air.local>
2025-01-31 17:00:40 +0530

    added middleware and shared types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,8 +10,12 @@ const App = () => {
 
  <Router  >
   <Routes>
-    <Route path="/" element={<Layout></Layout>} />
-    <Route path="/search" element={<>Search Page</>} /> 
+    <Route path="/" element={<Layout>
+      <p>Home Page</p>
+    </Layout>} />
+    <Route path="/search" element={<Layout>
+      <p>Search Page</p>
+    </Layout>} /> 
     <Route path="*" element={< Navigate to="/"/>} />
 
     
--- frontend/src/layouts/Layouts.tsx
+++ frontend/src/layouts/Layouts.tsx
@@ -3,11 +3,16 @@ import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 
 
-export const Layout = () => {
+interface Props {
+    children: React.ReactNode;
+}
+
+export const Layout = ({children}:Props) => {
     return ( <div className="flex flex-col min-h-screen ">
     <Header/>
     <Hero/>
-
+<div className="container mx-auto flex-1 py-10"> 
+    {children}  </div>
     <Footer/>
              
             </div>
